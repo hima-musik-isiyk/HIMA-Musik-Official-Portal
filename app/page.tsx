7db5e06816bd35ca3c3eb4476c4d2a584369dd8a
Vincent Adittama <adittamavincent@gmail.com>
2026-05-28 14:21:12 +0700

    feat: implement modular home page content fetching via Notion parent page block children

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { fetchBerandaModularDataCached } from "@/lib/notion";
+
 import Home from "../views/Home";
 
-export default function Page() {
-  return <Home />;
+export const revalidate = 60;
+
+export default async function Page() {
+  const pageId =
+    process.env.NOTION_BERANDA_PAGE_ID || "36e3b26dc3be8036be8cc142fa468964";
+  const data = await fetchBerandaModularDataCached(pageId);
+
+  return <Home heroSection={data?.heroSection} jelajahi={data?.jelajahi} />;
 }
